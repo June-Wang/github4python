d4463796fa51bdd044e10605cb797b69e019d7ec
Alex Wang <qq5910225@gmail.com>
2016-02-16 04:26:27 +0800

    修改判断条件

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ do
 	days=`echo $i+21|bc`
 	date_3week_ago=`date -d -${days}day +"%Y-%m-%d"`
 	my_day=`date -d -${i}day +"%Y-%m-%d"`
-	python3 000998.week.py "${my_day}" "${date_3week_ago}" >> ./final.txt
+	python3 check_stock_000998.py "${my_day}" "${date_3week_ago}" >> ./final.txt
 done
