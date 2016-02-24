6903075d6bd531fb5ca874e8765b04944b32c8c3
Alex Wang <804418881@qq.com>
2016-02-24 14:55:07 +0800

    写着玩儿

--- check_stock_000998.py
+++ check_stock_000998.py
@@ -6,6 +6,9 @@ my_day = sys.argv[1]
 #today = datetime.date.today()
 today = sys.argv[1]
 date_to3week_ago = sys.argv[2]
+date_to10days_ago = sys.argv[3]
+yestday = sys.argv[4]
+
 my_date = today
 
 #my_stock = ts.get_realtime_quotes('000998')
@@ -13,7 +16,9 @@ my_date = today
 #my_date = my_stock.date[0]
 
 df = ts.get_tick_data('000998',date=today)
-my_stock_price = (df.price.min() + df.price.max())/2
+my_stock_price_avg = (df.price.min() + df.price.max())/2
+my_stock_price_min = df.price.min()
+my_stock_price_max = df.price.max()
 
 #to3week_ago = today + datetime.timedelta(weeks=-3)
 #date_to3week_ago = to3week_ago.strftime('%Y-%m-%d')
@@ -25,12 +30,23 @@ my_stock_price = (df.price.min() + df.price.max())/2
 #print(my_pre_3week_num)
 #my_pre_1month = ts.get_hist_data('000998', ktype='D',start=str(date_to1month_ago),end=str(my_day))[['p_change']].sum()[0]
 
-if my_stock_price == my_stock_price:
-	sell_price = my_stock_price*20/100 + my_stock_price
+if my_stock_price_avg == my_stock_price_avg:
 	my_pre_3week = ts.get_hist_data('000998', ktype='D',start=str(date_to3week_ago),end=str(my_day))[['p_change']].sum()
 	my_pre_3week_num = my_pre_3week[0]
-	if my_pre_3week_num <= -25:
-		print(my_date+'\tp_change:\t'+str(my_pre_3week_num)+'\tprice:\t'+("%.2f" % my_stock_price)+'\tsell:\t'+("%.2f" % sell_price))
-	else:
-		print(my_date+'\tp_change:\t'+str(my_pre_3week_num)+'\tprice:\t'+("%.2f" % my_stock_price))
-#and my_pre_1month <= -20:
+	df = ts.get_tick_data('000998',date=yestday)
+	my_stock_price_avg_yestday = (df.price.min() + df.price.max())/2
+#	my_pre_date_to10days = ts.get_hist_data('000998', ktype='D',start=str(date_to10days_ago),end=str(my_day))[['p_change']].sum()
+#	my_pre_date_to10days_num = my_pre_date_to10days[0]
+	#green
+	if my_stock_price_avg_yestday > my_stock_price_avg:
+		if my_pre_3week_num <= -25: 
+			sell_price_20 = my_stock_price_avg*20/100 + my_stock_price_avg
+			print(my_date+' p_change:\t'+("%.2f" % my_pre_3week_num)+'\tprice_avg: '+("%.2f" % my_stock_price_avg)+'\tprice_min: '+("%.2f" % my_stock_price_min)+'\tprice_max: '+("%.2f" % my_stock_price_max)+'\tsell20: '+("%.2f" % sell_price_20))
+		elif my_pre_3week_num <= -20:
+			sell_price_15 = my_stock_price_avg*15/100 + my_stock_price_avg
+			print(my_date+' p_change:\t'+("%.2f" % my_pre_3week_num)+'\tprice_avg: '+("%.2f" % my_stock_price_avg)+'\tprice_min: '+("%.2f" % my_stock_price_min)+'\tprice_max: '+("%.2f" % my_stock_price_max)+'\tsell15: '+("%.2f" % sell_price_15))
+		elif my_pre_3week_num <= -15:
+			sell_price_10 = my_stock_price_avg*10/100 + my_stock_price_avg
+			print(my_date+' p_change:\t'+("%.2f" % my_pre_3week_num)+'\tprice_avg: '+("%.2f" % my_stock_price_avg)+'\tprice_min: '+("%.2f" % my_stock_price_min)+'\tprice_max: '+("%.2f" % my_stock_price_max)+'\tsell10: '+("%.2f" % sell_price_10))
+		else:	
+			print(my_date+' p_change:\t'+("%.2f" % my_pre_3week_num)+'\tprice_avg: '+("%.2f" % my_stock_price_avg)+'\tprice_min: '+("%.2f" % my_stock_price_min)+'\tprice_max: '+("%.2f" % my_stock_price_max))
--- test.sh
+++ test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-for i in `seq 180`
+for i in `seq 600`
 do
-	days=`echo $i+21|bc`
-	date_3week_ago=`date -d -${days}day +"%Y-%m-%d"`
+	date_20days=`echo $i+20|bc`
+	date_10days=`echo $i+10|bc`
+	date_yestday=`echo $i+1|bc`
+	date_3week_ago=`date -d -${date_20days}day +"%Y-%m-%d"`
+	date_10days_ago=`date -d -${date_10days}day +"%Y-%m-%d"`
+	date_yestday_time=`date -d -${date_yestday}day +"%Y-%m-%d"`
 	my_day=`date -d -${i}day +"%Y-%m-%d"`
-	python3 check_stock_000998.py "${my_day}" "${date_3week_ago}" >> ./final.txt
+	python3 check_stock_000998.py "${my_day}" "${date_3week_ago}" "${date_10days_ago}" "${date_yestday_time}">> ./final.txt
 done
