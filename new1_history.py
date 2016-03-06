1d0040e9686b1f092f0ab7e650ed3776b48724d8
Alex Wang <qq5910225@gmail.com>
2016-03-06 16:10:27 +0800

    ddd

--- new1_history.py
+++ new1_history.py
@@ -9,7 +9,7 @@ stock_code_p_change = float(sys.argv[2])
 
 now = datetime.date.today()
 
-for num in range(5):
+for num in range(200):
 	my_date = now - datetime.timedelta(days=num)
 	B1_days = num+1
 	B20_days = num+21
@@ -23,7 +23,6 @@ for num in range(5):
 		continue
 	df = ts.get_hist_data(stock_code,start=str(date_20days_ago),end=str(my_date))
 	price = df.close[0]
-	yestoday_price = df.close[1]
 	volume = df.volume[0]
 	#volume_avg20 = df.volume.sum()/len(df.volume)
 	#volume_avg20_change = (volume - volume_avg20)/volume_avg20 * 100
@@ -37,35 +36,45 @@ for num in range(5):
 	p_change_10_avg = (price - price_avg_10)/price_avg_10 * 100
 	p_change_20_avg = (price - price_avg_20)/price_avg_20 * 100
 
+	yestoday_price = df.close[1]
+	yestoday_price_avg_10 = df.ma10[1]
+	yestoday_p_change_10_avg = (yestoday_price - yestoday_price_avg_10)/yestoday_price_avg_10 * 100
+
 	df = ts.get_sina_dd(stock_code, date=str(my_date))
 	
-	types = set(df.type)
-	type_sum = df['volume'].sum()
-	type_sum_count = len(df)
-	#s_out = ''
-	stock_type_count = dict()
-	stock_type_change = dict()
-	stock_type_volume = dict()
-	for s_type in types:
-		stock_type_count[s_type] = df['type'][df.type == s_type].count()
-		stock_type_volume[s_type] = df['volume'][df.type == s_type].sum()
-		stock_type_change[s_type] = stock_type_volume[s_type]/type_sum*100
-
-	stock_type_count_msg = ''
-	stock_type_change_msg = ''
-	for s_type in types:
-		#stock_type_count_msg = stock_type_count_msg+'\t'+str(s_type)+'\t'+str(stock_type_count[s_type])+'\t'
-		stock_type_change_msg = stock_type_change_msg+' '+str(s_type)+' '+("%.2f" % stock_type_change[s_type])+'% '
-		
-	stock_type_change_msg = stock_type_change_msg + ' 总数: '+ str(type_sum_count)
+	try: 
+		#len(df.type)
+		types = sorted(set(df.type))
+		type_sum = df['volume'].sum()
+		type_sum_count = len(df)
+		#s_out = ''
+		stock_type_count = dict()
+		stock_type_change = dict()
+		stock_type_volume = dict()
+		for s_type in types:
+			stock_type_count[s_type] = df['type'][df.type == s_type].count()
+			stock_type_volume[s_type] = df['volume'][df.type == s_type].sum()
+			stock_type_change[s_type] = stock_type_volume[s_type]/type_sum*100
+	
+		stock_type_count_msg = ''
+		stock_type_change_msg = ''
+		for s_type in types:
+			#stock_type_count_msg = stock_type_count_msg+'\t'+str(s_type)+'\t'+str(stock_type_count[s_type])+'\t'
+			stock_type_change_msg = stock_type_change_msg+'\t'+str(s_type)+'\t'+("%.2f" % stock_type_change[s_type])+'%'
+			
+		stock_type_change_msg = stock_type_change_msg + ' 总数: '+ str(type_sum_count)
+	except:
+		#continue
+		stock_type_change_msg = ''
 	#print(stock_type_change_msg)
 
 	act_msg = ''
-	if p_change_5_avg < 0 and p_change_5_avg > p_change_10_avg and p_change_10_avg > p_change_20_avg:
+	#if p_change_5_avg < 0 and p_change_5_avg > p_change_10_avg and p_change_10_avg > p_change_20_avg and p_change_10_avg <= (stock_code_p_change*-100):
+	if p_change > p_change_5_avg and p_change_5_avg > p_change_10_avg and p_change_10_avg <= (stock_code_p_change*-1) and p_change_10_avg < yestoday_p_change_10_avg:
 		act_msg = 'buy'
 
 	date_now = str(my_date)
-	price_msg = 'price(now/5/10/20/): '+("%.2f" % price)+' '+("%.2f" % price_avg_10)+' '+("%.2f" % price_avg_20)
+	price_msg = 'price(1/5/10): '+("%.2f" % price)+' '+("%.2f" % price_avg_5)+' '+("%.2f" % price_avg_10)
 	#+' '+("%.2f" % price_min)+' '+("%.2f" % price_max)
-	p_change_msg = 'change(now/5/10/20):\t'+("%.2f" % p_change)+'\t'+("%.2f" % p_change_5_avg)+'\t'+("%.2f" % p_change_10_avg)+'\t'+("%.2f" % p_change_20_avg)
+	p_change_msg = 'change(1/5/10):\t'+("%.2f" % p_change)+'\t'+("%.2f" % p_change_5_avg)+'\t'+("%.2f" % p_change_10_avg)#+'\t'+("%.2f" % p_change_20_avg)
 	print(date_now+' '+price_msg+' '+p_change_msg+'\t'+stock_type_change_msg+' '+act_msg)
