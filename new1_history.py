d4470ee8b860717c6d24ceca5f486ad0ba929b1e
Alex Wang <qq5910225@gmail.com>
2016-03-08 09:59:05 +0800

    循环天数

--- new1_history.py
+++ new1_history.py
@@ -16,37 +16,58 @@ date_ago = now - datetime.timedelta(days=num4days)
 before_yestoday = now - datetime.timedelta(days=2)
 before_date_ago = now - datetime.timedelta(days=num4days+1)
 
-for1day = reversed(pd.bdate_range(start=str(date_ago),end=str(yestoday)))
-for2day = reversed(pd.bdate_range(start=str(before_date_ago),end=str(before_yestoday)))
+for1day = pd.bdate_range(start=str(date_ago),end=str(yestoday))
+for2day = pd.bdate_range(start=str(before_date_ago),end=str(before_yestoday))
 
-df = ts.get_hist_data(stock_code,start=str(before_date_ago),end=str(yestoday))
-
-#my_days = set(df.index)
-#my_days = reversed(sorted(set(df.index)))
+today_list = list()
+yestoday_list = list()
 
 for day in for1day:
+	today_list.append(str(day.date()))
+
+for day in for2day:
+	yestoday_list.append(str(day.date()))
+
+#sys.exit(1)
+
+df = ts.get_hist_data(stock_code,start=str(date_ago),end=str(yestoday))
+
+#for day in reversed(for1day):
+for day in today_list:
+	date_today = today_list.pop()
+	date_yestoday = yestoday_list.pop()
+
 	try:
-		price = df[df.index == str(day.date())].close[0]
+		price = df[df.index == date_today].close[0]
+		yestoday_price = df[df.index == date_yestoday].close[0]
+		
 	except:
 		continue
 
 	if price != price:
 		continue
-	volume = df.volume[0]
-	price_min = df.low[0]
-	price_max = df.high[0]
-	price_avg_5 = df.ma5[0]
-	price_avg_10 = df.ma10[0]
-	price_avg_20 = df.ma20[0]
-	p_change = df.p_change[0]
+
+	volume = df[df.index == date_today].volume[0]
+	price_min = df[df.index == date_today].low[0]
+	price_max = df[df.index == date_today].high[0]
+	price_avg_5 = df[df.index == date_today].ma5[0]
+	price_avg_10 = df[df.index == date_today].ma10[0]
+	price_avg_20 = df[df.index == date_today].ma20[0]
+	p_change = df[df.index == date_today].p_change[0]
 	p_change_avg_5 = (price - price_avg_5)/price_avg_5 * 100
 	p_change_avg_10 = (price - price_avg_10)/price_avg_10 * 100
 	p_change_avg_20 = (price - price_avg_20)/price_avg_20 * 100
 
+	yestoday_price_avg_5 = df[df.index == date_yestoday].ma5[0]
+	yestoday_price_avg_10 = df[df.index == date_yestoday].ma10[0]
+	yestoday_p_change_avg_5 = (yestoday_price - yestoday_price_avg_5)/yestoday_price_avg_5 * 100
+	yestoday_p_change_avg_10 = (yestoday_price - yestoday_price_avg_10)/yestoday_price_avg_10 * 100
+	
+	#print(date_today,date_yestoday)
+	#continue
+
 	act_msg = ''
-	#my_date = now - datetime.timedelta(days=num)
-	my_date = str(day)
-	date_now = str(my_date)
+	date_now = date_today
 	price_msg = 'price(1/5/10/20/min/max):\t'+("%.2f" % price)+'\t'+("%.2f" % price_avg_5)+'\t'+("%.2f" % price_avg_10)+'\t'+("%.2f" % price_avg_20)+'\t'+("%.2f" % price_min)+'\t'+("%.2f" % price_max)
 	p_change_msg = 'change(1/5/10):\t'+("%.2f" % p_change)+'\t'+("%.2f" % p_change_avg_5)+'\t'+("%.2f" % p_change_avg_10)#+'\t'+("%.2f" % p_change_avg_20)
 	print(date_now+' '+price_msg+' '+p_change_msg+'\t'+ ' '+act_msg)
