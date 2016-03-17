c851af4548822e62f8041cea2a74d691f0aa4728
Alex Wang <qq5910225@gmail.com>
2016-03-17 17:05:53 +0800

    ddd

--- new2_history.py
+++ new2_history.py
@@ -10,12 +10,12 @@ from colorama import Fore, Back, Style
 colorama.init()
 
 stock_code = sys.argv[1]
-stock_code_p_change = float(sys.argv[2])
+#stock_code_p_change = float(sys.argv[2])
 
-num4days = 100
+num4days = 1200
 now = datetime.date.today()
 yestoday = now - datetime.timedelta(days=1)
-end_day = now - datetime.timedelta(days=num4days)
+end_day = now - datetime.timedelta(days=num4days+60)
 workday = pd.bdate_range(start=str(end_day),end=str(yestoday))
 
 try:
@@ -26,7 +26,26 @@ except:
 
 days = len(workday.date)
 
-for i in range(days-1,10,-1):
+def get_day(day,loop_i):
+	global workday
+	num = day+1
+	my_date_tmp = list()
+	for my_day in range(loop_i-1,loop_i-num,-1):
+		my_date_tmp.append(str(workday.date[my_day]))
+	return(my_date_tmp)
+
+def get_p_change_for_days(date_list):
+	global df
+	my_change_sum = 0.0
+	for my_date in date_list:
+		try:
+			my_change_tmp = float(df[df.index == my_date].p_change[0])
+		except:
+			my_change_tmp = 0.0
+		my_change_sum += my_change_tmp
+	return(my_change_sum)
+			
+for i in range(days-1,60,-1):
 	my_str = ''
 	date_today = str(workday.date[i])
 	date_yestoday = str(workday.date[i-1])
@@ -51,34 +70,17 @@ for i in range(days-1,10,-1):
 	price_avg_10 = df[df.index == date_today].ma10[0]
 	price_avg_20 = df[df.index == date_today].ma20[0]
 
-	p_change_3 = 0.0
-	for f3 in range(i-1,i-4,-1):
-		my_date = str(workday.date[f3])
-		try:
-			my_change = float(df[df.index == my_date].p_change[0])
-		except:
-			my_change = 0.0
-		p_change_3 = p_change_3 + my_change
-
-	p_change_5 = 0.0
-	for f5 in range(i-1,i-6,-1):
-		my_date = str(workday.date[f5])
-		try:
-			my_change = float(df[df.index == my_date].p_change[0])
-		except:
-			my_change = 0.0
-		p_change_5 = p_change_5 + my_change
-
-	p_change_10 = 0.0
-	for f10 in range(i-1,i-11,-1):
-		my_date = str(workday.date[f10])
-		try:
-			my_change = float(df[df.index == my_date].p_change[0])
-		except:
-			my_change = 0.0
-		p_change_10 = p_change_10 + my_change
+	p_change_3 = get_p_change_for_days(get_day(3,i))
+	p_change_5 = get_p_change_for_days(get_day(5,i))
+	p_change_7 = get_p_change_for_days(get_day(7,i))
+	p_change_10 = get_p_change_for_days(get_day(10,i))
+	p_change_15 = get_p_change_for_days(get_day(15,i))
+	p_change_20 = get_p_change_for_days(get_day(20,i))
+	p_change_30 = get_p_change_for_days(get_day(30,i))
+	p_change_60 = get_p_change_for_days(get_day(60,i))
 
 	p_change = df[df.index == date_today].p_change[0]
+	#p_change = df[df.index == date_yestoday].p_change[0]
 	p_change_min = (price_min - price_open)/price_open * 100
 	p_change_max = (price_max - price_open)/price_open * 100
 	p_change_open = p_change_max + p_change_min
@@ -94,16 +96,16 @@ for i in range(days-1,10,-1):
 	yestoday_p_change_avg_5 = (yestoday_price_close - yestoday_price_avg_5)/yestoday_price_avg_5 * 100
 	yestoday_p_change_avg_10 = (yestoday_price_close - yestoday_price_avg_10)/yestoday_price_avg_10 * 100
 	
-#	if p_change > p_change_avg_10 and p_change_avg_5 < 0 and p_change > p_change_avg_5 and p_change_avg_5 > p_change_avg_10 and  yestoday_p_change_avg_10 <= (stock_code_p_change*-1) and p_change_avg_10 > yestoday_p_change_avg_10:
-
-	price_msg = 'price(open/close/5/10/min/max):\t'+("%.2f" % price_open)+'\t'+("%.2f" % price_close)+'\t'+("%.2f" % price_avg_5)+'\t'+("%.2f" % price_avg_10)+'\t'+("%.2f" % price_min)+'\t'+("%.2f" % price_max)
-	p_change_msg = 'change(1/5/10/open/close/min/max):\t'+("%.2f" % p_change)+'\t'+("%.2f" % p_change_avg_5)+'\t'+("%.2f" % p_change_avg_10)+'\t'+("%.2f" % p_change_open) +'\t'+("%.2f" % p_change_close)+'\t'+("%.2f" % p_change_min) +'\t'+("%.2f" % p_change_max) 
-	p_change_msg = 'change(1/3/5/10):\t'+("%.2f" % p_change)+'\t'+("%.2f" % p_change_3)+'\t'+("%.2f" % p_change_5)+'\t'+("%.2f" % p_change_10)#+'\t'+("%.2f" % p_change_open) +'\t'+("%.2f" % p_change_close)+'\t'+("%.2f" % p_change_min) +'\t'+("%.2f" % p_change_max) 
+	price_msg = 'price(close/min/max): '+("%.2f" % price_close)+' '+("%.2f" % price_min)+' '+("%.2f" % price_max)
 	
-	if p_change_min < -6 and p_change_avg_5 < -5 and price_avg_10 > price_open:
-		print(Fore.YELLOW+date_now+' '+price_msg+' '+p_change_msg+Style.RESET_ALL)
-	elif p_change_max < 1 and p_change_max > -1 and p_change < -4 and  p_change_min < -4:
+	p_change_msg = 'change(1/3/7/10/15/20/30/60):\t'+("%.2f" % p_change)+'\t'+("%.2f" % p_change_3)+'\t'+("%.2f" % p_change_7)+'\t'+("%.2f" % p_change_10)+'\t'+("%.2f" % p_change_15)+'\t'+("%.2f" % p_change_20)+'\t'+("%.2f" % p_change_30)+'\t'+("%.2f" % p_change_60)
+	
+	#if p_change_min < -6 and p_change_avg_5 < -5 and price_avg_10 > price_open:
+	if p_change > -9 and p_change < -3 and ((p_change_3 < -9 and p_change_10 > -20) or (p_change_10 < -10 and p_change_10 > -20)):
 		print(Fore.CYAN+date_now+' '+price_msg+' '+p_change_msg+Style.RESET_ALL)
+	#elif p_change_max < 1 and p_change_max > -1 and p_change < -4 and  p_change_min < -4:
+	elif p_change_3 > 15 or p_change_10 >15:
+		print(Fore.YELLOW+date_now+' '+price_msg+' '+p_change_msg+Style.RESET_ALL)
 	elif p_change > 0:
 		print(Fore.RED+date_now+' '+price_msg+' '+p_change_msg+Style.RESET_ALL)
 	elif p_change < 0:
