5e4c0a4e2bf52ce45057d2a9f7c677967a442ecf
Alex Wang <qq5910225@gmail.com>
2016-04-17 23:12:08 +0800

    ddd

--- search_stock.py
+++ search_stock.py
@@ -27,7 +27,12 @@ df_basics = ts.get_stock_basics()
 #df_basics_list = list(df_basics[df_basics.totals < 500000].index)
 df_basics_list = list(df_basics[df_basics.totals >0].index)
 
-df_profit = ts.get_profit_data(2015,4)
+#df_profit = ts.get_profit_data(2014,4)
+df_profit = ts.get_profit_data(2015,1)
+#df_profit = ts.get_profit_data(2015,2)
+#df_profit = ts.get_profit_data(2015,3)
+#df_profit = ts.get_profit_data(2015,4)
+#df_profit = ts.get_profit_data(2016,1)
 #code,代码
 #name,名称
 #roe,净资产收益率(%)
@@ -38,9 +43,15 @@ df_profit = ts.get_profit_data(2015,4)
 #business_income,营业收入(百万元)
 #bips,每股主营业务收入(元)
 
-df_profit_list = list(df_profit[df_profit.gross_profit_rate>=30][df_profit.roe >=5].code)
+#df_profit_list = list(df_profit[df_profit.gross_profit_rate>=35][df_profit.roe >=5].code)
+df_profit_list = list(df_profit[df_profit.gross_profit_rate>=30][df_profit.net_profit_ratio >30].code)
 
-df_growth = ts.get_growth_data(2015,4)
+#df_growth = ts.get_growth_data(2014,4)
+df_growth = ts.get_growth_data(2015,1)
+#df_growth = ts.get_growth_data(2015,2)
+#df_growth = ts.get_growth_data(2015,3)
+#df_growth = ts.get_growth_data(2015,4)
+#df_growth = ts.get_growth_data(2016,1)
 #code,代码
 #name,名称
 #mbrg,主营业务收入增长率(%)
@@ -50,8 +61,8 @@ df_growth = ts.get_growth_data(2015,4)
 #epsg,每股收益增长率
 #seg,股东权益增长率
 
-#growth = df_growth[df_growth.mbrg >0][df_growth.nprg >0][df_growth.nav>0][df_growth.targ>0][df_growth.epsg>0][df_growth.seg>0].code
-df_growth_list = list(df_growth[df_growth.mbrg >= 90][df_growth.nprg >0].code)
+df_growth_list = list(df_growth[df_growth.mbrg >10][df_growth.nprg >10][df_growth.nav>10][df_growth.targ>0][df_growth.epsg>0].code)
+#df_growth_list = list(df_growth[df_growth.mbrg >= 90][df_growth.nprg >0].code)
 
 df_today = ts.get_today_all()
 #code：代码
@@ -65,7 +76,8 @@ df_today = ts.get_today_all()
 #volume:成交量
 #turnoverratio:换手率
 
-df_price_list = list(df_today[df_today.trade < 15][df_today.trade >0].code)
+#df_price_list = list(df_today[df_today.trade < 100][df_today.trade >0].code)
+df_price_list = list(df_today[df_today.trade >0].code)
 
 code_list = list()
 #df_basics_list
@@ -84,8 +96,9 @@ for code in set(code_list):
         stock_name = str(stock.name.values[0])
         stock_close = str("%.2f" % price_close)
         stock_industry = str(stock_basics.industry.values[0])
-        stock_mbrg = str(stock.mbrg.values[0])
-        print(stock_code+"\t"+stock_name+"\t"+stock_close+"\t"+stock_industry+"\t"+stock_mbrg)
+        #stock_mbrg = str(stock.mbrg.values[0])
+        #print(stock_code+"\t"+stock_name+"\t"+stock_close+"\t"+stock_industry+"\t"+stock_mbrg)
+        print(stock_code+","+stock_name+","+stock_close+","+stock_industry)
         num+=1
         #time.sleep(1)
 print(num)
