816db5ead9a768f212de17cab9c9922cd25d56cb
Alex Wang <qq5910225@gmail.com>
2016-04-18 16:40:49 +0800

    ddd

---
+++ basic2csv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.4
+
+import sys
+import tushare as ts
+
+path = '../csv/'
+for year in range(2005,2016):
+    for season in  range(1,5):
+        print(year,season)
+        df_profit = ts.get_profit_data(year,season)
+        df_growth = ts.get_growth_data(year,season)
+        df_operation = ts.get_operation_data(year,season)
+        df_debtpaying = ts.get_debtpaying_data(year,season)
+        df_cashflow = ts.get_cashflow_data(year,season)
+        df_report = ts.get_report_data(year,season)
+        filename = path+'profit'+str(year)+'_'+str(season)+'.csv'
+        df_profit.to_csv(filename)
+        filename = path+'growth'+str(year)+'_'+str(season)+'.csv'
+        df_growth.to_csv(filename)
+        filename = path+'operation'+str(year)+'_'+str(season)+'.csv'
+        df_operation.to_csv(filename)
+        filename = path+'debtpaying'+str(year)+'_'+str(season)+'.csv'
+        df_debtpaying.to_csv(filename)
+        filename = path+'cashflow'+str(year)+'_'+str(season)+'.csv'
+        df_cashflow.to_csv(filename)
+        filename = path+'report'+str(year)+'_'+str(season)+'.csv'
+        df_report.to_csv(filename)
--- basic2mysql.py
+++ basic2mysql.py
@@ -5,16 +5,28 @@ import tushare as ts
 from sqlalchemy import create_engine
 import mysql.connector
 
-df = ts.get_tick_data('600848', date='2014-12-22')
-#engine = create_engine('mysql://stockadmin:stock2016@127.0.0.1/stock?charset=utf8')
 conn = create_engine('mysql+mysqlconnector://stockadmin:stock2016@localhost/stock?charset=utf8')
 
-try:
-#存入数据库
-    df.to_sql('tick_data',conn)
-except mysql.connector.errors.ProgrammingErro as msg:
-    print(msg)
-    sys.exit(1)
+for year in range(2005,2016):
+    for season in  range(1,5):
+        print(year,season)
+        df_profit = ts.get_profit_data(year,season)
+        df_growth = ts.get_growth_data(year,season)
+        df_operation = ts.get_operation_data(year,season)
+        df_debtpaying = ts.get_debtpaying_data(year,season)
+        df_cashflow = ts.get_cashflow_data(year,season)
+        df_report = ts.get_report_data(year,season)
+        try:
+            df_profit.to_sql('profit',conn,if_exists='append')
+        except:
+            continue
+        df_growth.to_sql('growth',conn,if_exists='append')
+        df_operation.to_sql('operation',conn,if_exists='append')
+        df_debtpaying.to_sql('debtpaying',conn,if_exists='append')
+        df_cashflow.to_sql('cashflow',conn,if_exists='append')
+        df_report.to_sql('report',conn,if_exists='append')
+
+#sys.exit(1)
 
 #追加数据到现有表
 #df.to_sql('tick_data',engine,if_exists='append')
