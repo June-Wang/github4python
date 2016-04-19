d83c8c99c4a3f709fb7a65a388b022b8e3460968
Alex Wang <qq5910225@gmail.com>
2016-04-20 06:37:27 +0800

    ddd

--- basic2csv.py
+++ basic2csv.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3.4
 
 import sys
+import time
 import tushare as ts
 
 path = '../csv/'
-for year in range(2005,2016):
-    for season in  range(1,5):
+for year in range(2014,2015):
+    for season in  range(3,5):
         print(year,season)
         df_profit = ts.get_profit_data(year,season)
+        time.sleep(15)
         df_growth = ts.get_growth_data(year,season)
+        time.sleep(15)
         df_operation = ts.get_operation_data(year,season)
+        time.sleep(15)
         df_debtpaying = ts.get_debtpaying_data(year,season)
+        time.sleep(15)
         df_cashflow = ts.get_cashflow_data(year,season)
+        time.sleep(15)
         df_report = ts.get_report_data(year,season)
+        time.sleep(15)
         filename = path+'profit'+str(year)+'_'+str(season)+'.csv'
         df_profit.to_csv(filename)
         filename = path+'growth'+str(year)+'_'+str(season)+'.csv'
--- basic2mysql.py
+++ basic2mysql.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3.4
 
 import sys
+import time
 import tushare as ts
 from sqlalchemy import create_engine
 import mysql.connector
 
 conn = create_engine('mysql+mysqlconnector://stockadmin:stock2016@localhost/stock?charset=utf8')
 
-for year in range(2005,2016):
+for year in range(2014,2016):
     for season in  range(1,5):
         print(year,season)
         df_profit = ts.get_profit_data(year,season)
+        time.sleep(15)
         df_growth = ts.get_growth_data(year,season)
+        time.sleep(15)
         df_operation = ts.get_operation_data(year,season)
+        time.sleep(15)
         df_debtpaying = ts.get_debtpaying_data(year,season)
+        time.sleep(15)
         df_cashflow = ts.get_cashflow_data(year,season)
+        time.sleep(15)
         df_report = ts.get_report_data(year,season)
-        try:
-            df_profit.to_sql('profit',conn,if_exists='append')
-        except:
-            continue
+        df_profit.to_sql('profit',conn,if_exists='append')
         df_growth.to_sql('growth',conn,if_exists='append')
         df_operation.to_sql('operation',conn,if_exists='append')
         df_debtpaying.to_sql('debtpaying',conn,if_exists='append')
