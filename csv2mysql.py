d57a99fb4390a9ab9c50669ae52a8641f698fb3c
Alex Wang <qq5910225@gmail.com>
2016-04-20 17:32:01 +0800

    我就日了

--- csv2mysql.py
+++ csv2mysql.py
@@ -5,27 +5,16 @@ import csv
 import sqlalchemy as sa
 import mysql.connector
 
-csvin = 'cashflow'
+#csvin = 'cashflow'
+csvin = 'report'
 years = range(2012,2016)
 seasons = range(1,5)
 cells = [(year,season) for year in years for season in seasons]
-
-user = 'stockadmin'
-host = 'localhost'
-password = 'stock2016'
-database = 'stock'
 table = csvin
 
-mysql_config = {
-'user': user,
-'host': host,
-'password': password,
-'database': database
-}
-
-conn = mysql.connector.connect(**mysql_config)
-cursor = conn.cursor()
-
+conn = sa.create_engine('mysql+mysqlconnector://stockadmin:stock2016@localhost/stock')
+#conn = mysql.connector.connect(**mysql_config)
+#cursor = conn.cursor()
 
 for year,season in cells:
     print(year,season)
@@ -39,24 +28,36 @@ for year,season in cells:
         sys.exit(1)
 
     db_field_list = cin.fieldnames[1:]
+#    print(db_field_list)
     
     for row in rows:
         data_value_list = list()
+
+        #marks = ', '.join('?' * len(myDict))
+        #qry = "Insert Into Table (%s) Values (%s)" % (qmarks, qmarks)
+        #cursor.execute(qry, myDict.keys() + myDict.values())
+
         for field in db_field_list:
             data_value_list.append(row[field])
 
-        db_field_list.extend(['year','season'])
+        #db_field_list.extend(['year','season'])
+        db_field_list_new = [row for row in db_field_list]
+        db_field_list_new.extend(['year','season'])
+#        print(db_field_list_new)
+
         year_str = str(year)
         season_str = str(season)
-        data_value_list.extend([year_str,season_str])
-        str_s = ''
-        str_s = len(db_field_list)*',%s'
 
-        db_field_str = ','.join(db_field_list)
-        
-        sql = 'INSERT INTO '+table+' '+ '('+db_field_str+')'+' VALUES '+'('+str_s[1:]+');'
-        print(sql)
-        #sys.exit(1)
-        cursor.execute(sql,data_value_list)
+        data_value_list_new = [row for row in data_value_list]
+        data_value_list_new.extend([year_str,season_str])
 
+        str_s = len(db_field_list_new)*',%s'
+        marks = str_s[1:]
+        db_field_list_new_str = ', '.join(db_field_list_new)
+        data_value_list_new_str = str(data_value_list_new).replace('[', '').replace(']', '')
+
+        qry = "Insert Into %s (%s) Values (%s);" % (table,db_field_list_new_str,data_value_list_new_str)
+        #print(qry)
+        #cursor.execute(qry)
+        conn.execute(qry)
 #cursor.close()
