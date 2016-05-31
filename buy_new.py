ba13b89246dd5cfb8586a77d2430258ff5111fd3
Alex Wang <qq5910225@gmail.com>
2016-05-31 15:21:35 +0800

    ddd

--- buy_new.py
+++ buy_new.py
@@ -11,6 +11,8 @@ import time
 
 colorama.init()
 
+day_list = [3,5,10,15,20,30,60,90,120]
+
 try:
 	basics = ts.get_stock_basics()
 except:
@@ -25,11 +27,11 @@ def get_day(day,loop_i,workday):
 		my_date_tmp.append(str(workday.date[my_day]))
 	return(my_date_tmp)
 
-def get_p_change_for_days(date_list,df):
+def get_p_change_for_days(date_list,df,day_list):
 	#global df
 	my_change_sum = 0.0
 	count = 0
-	day_list = [3,5,10,15,20,30,60,90,120]
+	#day_list = [3,5,10,15,20,30,60,90,120]
 	data_list = list()
 	for my_date in date_list:
 		try:
@@ -42,6 +44,13 @@ def get_p_change_for_days(date_list,df):
 			#print(count)
 			data_list.append(my_change_sum)
 	return(data_list)
+
+def get_day_data(p_change_list,day_list):
+	day_data = dict()
+	#day_list = [3,5,10,15,20,30,60,90,120]
+	for day,data in zip(day_list,p_change_list):
+		day_data[day] = data
+	return(day_data)
 
 def get_color(text):
 	my_number = float(text)
@@ -92,12 +101,10 @@ for code in stock_list:
 		price_max = df[df.index == date_today].high[0]
 		p_change = df[df.index == date_today].p_change[0]
 	
-		p_change_list = get_p_change_for_days(get_day(121,i,workday),df)
+		p_change_list = get_p_change_for_days(get_day(121,i,workday),df,day_list)
 	
-		day_data = dict()
-		day_list = [3,5,10,15,20,30,60,90,120]
-		for day,data in zip(day_list,p_change_list):
-			day_data[day] = data
+		day_data = get_day_data(p_change_list,day_list)
+		#print(stock_code,day_data[5],day_data[10])
 
 		price_msg = 'P(min/max):\t'+("%.2f" % price_min)+' '+("%.2f" % price_max)
 		p_change_title = 'C(1/3/5/10/15/20/30/60/90/120):\t'
@@ -111,10 +118,10 @@ for code in stock_list:
 			print(stock_code +" "+stock_name+"\t"+Fore.CYAN+date_now+' '+price_msg+' '+p_change_title+Style.RESET_ALL+p_change_msg)
 		if day_data[5] > -15 or price_open >= 15 or day_data[3] > 0 or day_data[5] > 0:
 			continue
-		#if p_change < 0 and day_data[3] < 0 and day_data[5] < 0 and day_data[10] < 0 and day_data[15] < 0  and day_data[20] < 0 and day_data[30] < 0 and day_data[60] < 0 and day_data[90] < 0 and day_data[120] < 0:
+		if day_data[3] > 20:
+			print(stock_code +" "+stock_name+"\t"+Fore.CYAN+date_now+' '+price_msg+' '+p_change_title+Style.RESET_ALL+p_change_msg)
 		if day_data[10] < 0 and day_data[15] < 0  and day_data[20] < 0 and day_data[30] < 0 and day_data[60] < 0 and day_data[90] < 0 and day_data[120] < 0:
 			print(stock_code +" "+stock_name+"\t"+Fore.CYAN+date_now+' '+price_msg+' '+p_change_title+Style.RESET_ALL+p_change_msg)
-			#time.sleep(2)
 	#	elif p_change < 0 and day_data[3] > 0 and day_data[5] >0 and day_data[10] >0 and day_data[20] >0 and day_data[30] >0 and day_data[60] >0:
 	#		print(Fore.YELLOW+date_now+' '+price_msg+' '+p_change_title+Style.RESET_ALL+p_change_msg)
 	#	elif p_change > 0:
