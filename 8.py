fbd312929769aa676d87770cc6dfd0ce7a4a4037
Alex Wang <qq5910225@gmail.com>
2016-06-16 14:20:53 +0800

    ddd

--- 8.py
+++ 8.py
@@ -10,40 +10,35 @@ from termcolor import colored, cprint
 import time
 import multiprocessing
 
+def get_color(text):
+	my_number = float(text)
+	if my_number > 0:
+		my_text = colored(text, 'red')
+	elif my_number < 0:
+		my_text = colored(text, 'green')
+	else:
+		my_text = text
+	return(my_text)
+
 def get_basics_info(code,basics):
 	stock_basics = {}
 	name = str(basics[basics.index == code][['name']].values[0][0])
 	industry = str(basics[basics.index == code][['industry']].values[0][0]) #行业
 	area = str(basics[basics.index == code][['area']].values[0][0]) #区域
 	pe = str(basics[basics.index == code][['pe']].values[0][0]) #市盈率
-	pb = str(basics[basics.index == code][['pb']].values[0][0]) #市净率
+	try:
+		pb = str(basics[basics.index == code][['pb']].values[0][0]) #市净率
+	except:
+		pb = 0.0
 	stock_basics[code] = {'name':name,'industry':industry,'area':area,'pe':pe,'pb':pb}
 	return(stock_basics[code])
 
-def do_it(code,basics):
-
+def p_change_persent(df):
 	count = 0
-	num4days = 200
-	now = datetime.date.today()
-	yestoday = now - datetime.timedelta(days=1)
-	end_day = now - datetime.timedelta(days=num4days)
-	#workday = pd.bdate_range(start=str(end_day),end=str(yestoday))
-
-	stock_basics_dict = {}
-	stock_basics_dict[code] = get_basics_info(code,basics)
-	print(p_change,code,stock_basics_dict[code])
-
-	try:
-		df = ts.get_hist_data(stock_code,start=str(end_day),end=str(yestoday))
-	except:
-		print('timeout!')
-		sys.exit(1)
-
-	#days = len(workday.date)
-
+	num4up = 0
 	for date_today in df.index.values:
 		date_yestoday=str(datetime.datetime.strptime(date_today, '%Y-%m-%d') + datetime.timedelta(days = -1))[:10]
-		print(date_today,date_yestoday)
+		#print(date_today,date_yestoday)
 		try:
 			p_change = df[df.index == date_today].p_change[0]
 		except:
@@ -52,10 +47,40 @@ def do_it(code,basics):
 		if p_change != p_change:
 			continue
 		
-		sys.exit()
-		#count +=1
-		#if p_change >3:
-		#	num
+		count +=1
+		if p_change >0:
+			num4up +=1
+	up_persent = num4up/count*100
+	return(up_persent)
+
+def do_it(code,basics):
+
+	num4days = 15
+	now = datetime.date.today()
+	yestoday = now - datetime.timedelta(days=1)
+	end_day = now - datetime.timedelta(days=num4days)
+
+	stock_basics_dict = {}
+	stock_basics_dict[code] = get_basics_info(code,basics)
+
+	try:
+		df = ts.get_hist_data(code,start=str(end_day),end=str(yestoday))
+	except:
+		print('timeout!')
+		sys.exit(1)
+
+	up_persent = p_change_persent(df)
+	down_persent = -100.00+up_persent
+
+	if up_persent >= 80:
+		msg_head = get_color(("%.2f" % up_persent))
+	elif down_persent <= -80:
+		msg_head = get_color(("%.2f" % down_persent))
+
+	msg_mid = code+'\t'+stock_basics_dict[code]['name']
+	#	msg_head = '[up/down]\t'+get_color(("%.2f" % up_persent))+'\t'+get_color(("%.2f" % down_persent))
+	msg_end = '行业\t'+stock_basics_dict[code]['industry']+'\t'+'市盈率\t'+stock_basics_dict[code]['pe']
+	print(msg_head+'\t'+msg_mid+'\t'+msg_end)
 
 if __name__ == "__main__":
 
