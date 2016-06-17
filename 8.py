a25f5306474583d266cf97c86a5cd79bf0a99b71
Alex Wang <qq5910225@gmail.com>
2016-06-17 16:02:55 +0800

    ddd

--- 8.py
+++ 8.py
@@ -20,6 +20,17 @@ def get_color(text):
 		my_text = text
 	return(my_text)
 
+def get_price_info(code,df):
+	date = df.index.values[0]
+	price = {}
+	price_open = df[df.index == date].open[0] #开盘价格
+	price_close = df[df.index == date].close[0] #开盘价格
+	price_min = df[df.index == date].low[0] #当日最低
+	price_max = df[df.index == date].high[0] #当日最高
+	p_change = df[df.index == date].p_change[0] #当日股票涨幅
+	price[code] = {'open':price_open,'close':price_close,'min':price_min,'max':price_max,'p_change':p_change}
+	return(price[code])
+
 def get_basics_info(code,basics):
 	stock_basics = {}
 	name = str(basics[basics.index == code][['name']].values[0][0])
@@ -69,18 +80,20 @@ def do_it(code,basics):
 		print('timeout!')
 		sys.exit(1)
 
+	price_dict = get_price_info(code,df)
+	price_close = price_dict['close']
+	#print(str(price_close))
+
 	up_persent = p_change_persent(df)
 	down_persent = -100.00+up_persent
 
 	if up_persent >= 40:
 		msg_head = get_color(("%.2f" % up_persent))
 		msg_mid = code+'\t'+stock_basics_dict[code]['name']
-		msg_end = '行业\t'+stock_basics_dict[code]['industry']+'\t'+'市盈率\t'+stock_basics_dict[code]['pe']
+		msg_end = '价格\t'+str(price_close)+'\t行业\t'+stock_basics_dict[code]['industry']+'\t'+'市盈率\t'+stock_basics_dict[code]['pe']
 		print(msg_head+'\t'+msg_mid+'\t'+msg_end)
 	elif down_persent <= -80:
 		msg_head = get_color(("%.2f" % down_persent))
-
-	#	msg_head = '[up/down]\t'+get_color(("%.2f" % up_persent))+'\t'+get_color(("%.2f" % down_persent))
 
 if __name__ == "__main__":
 
