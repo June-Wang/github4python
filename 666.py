4d0d0e9aca0fd5dec4f991b3de258d2de0980860
Alex Wang <qq5910225@gmail.com>
2016-06-17 14:21:33 +0800

    ddd

--- 666.py
+++ 666.py
@@ -186,7 +186,7 @@ if __name__ == "__main__":
 
 	colorama.init()
 	stock_code = sys.argv[1]
-	num4days = 300
+	num4days = 200
 	day_list = [3,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120] #取样时间列表
 
 	now = datetime.date.today()
