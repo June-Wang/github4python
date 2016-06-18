463d8c8afc0d92d75ea8a2fe51ebba62bf1b8914
Alex Wang <qq5910225@gmail.com>
2016-06-18 10:52:40 +0800

    ddd

--- 666.py
+++ 666.py
@@ -197,12 +197,12 @@ if __name__ == "__main__":
 
 	colorama.init()
 	stock_code = sys.argv[1]
-	num4days = 200
-	day_list = [3,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120] #取样时间列表
+	num4days = 400
+	day_list = [3,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125] #取样时间列表
 
 	now = datetime.date.today()
 	yestoday = now - datetime.timedelta(days=1)
-	end_day = now - datetime.timedelta(days=num4days*2)
+	end_day = now - datetime.timedelta(days=num4days+day_list[-1])
 
 	try:
 		stock_basics = ts.get_stock_basics()
