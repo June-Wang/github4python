710f00dc11a10efbc1e1ab15de1daae611aeb1d1
Alex Wang <qq5910225@gmail.com>
2016-06-24 13:22:46 +0800

    dd

--- 666.py
+++ 666.py
@@ -127,14 +127,14 @@ def color4msg(df,code,day_count,stock_basics_dict,price_dict,sh_price_dict,perse
 	end_msg = persent_msg+'\t'+p_change_msg
 
 	#if (persent <-90 and price_dict[code]['p_change'] > -3 ) or (persent <-50 and sh_persent <=-90 and price_dict[code]['p_change'] > -3):
-	if (persent <=-90 and persent>=-80 ) or (persent <-80 and sh_persent <=-90):
+	if persent <=-85:
 		color('cyan',mid_msg,end_msg)
 	elif persent <= -50 and sh_persent <= -40:
 		if price_dict[code]['p_change'] > 0:
 			color('red',mid_msg,end_msg)
 		elif price_dict[code]['p_change'] < 0:
 			color('green',mid_msg,end_msg)
-	elif (persent >= -80 and persent <= -60) or (persent <=-40 and sh_persent <=-80):
+	elif (persent >= -85 and persent <= -65) or (persent <=-40 and sh_persent <=-80):
 	#elif (persent <= -60 and sh_persent <= -60) or (persent <=-40 and sh_persent <=-90):
 		color('magenta',mid_msg,end_msg)
 	elif persent>=50 and sh_persent >=50:
