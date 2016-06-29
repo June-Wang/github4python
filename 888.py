e69d053088d7628466915a3b4e563f69dff53b82
Alex Wang <qq5910225@gmail.com>
2016-06-29 11:51:13 +0800

    ddd

--- 888.py
+++ 888.py
@@ -136,7 +136,6 @@ if __name__ == "__main__":
 	colorama.init()
 
 	num4days = 300
-	#day_list = [3,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120] #取样时间列表
 	day_list = [i for i in range(5,185,5)]
 	day_list.append(3)
 
@@ -149,7 +148,6 @@ if __name__ == "__main__":
 	else:
 		yestoday = now - datetime.timedelta(days=1)
 
-	#yestoday = now - datetime.timedelta(days=1)
 	end_day = now - datetime.timedelta(days=num4days+max(day_list)+100)
 
 
