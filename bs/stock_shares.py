ce1f7ee1937f6ceced5a77746e90217222532e8a
Alex Wang <qq5910225@gmail.com>
2016-07-04 14:49:37 +0800

    ddd

--- bs/stock_shares.py
+++ bs/stock_shares.py
@@ -6,7 +6,19 @@ import os
 import requests
 import pandas as pd
 import tushare as ts
+import multiprocessing
 #from bs4 import BeautifulSoup
+
+def do_it(stock_code,stock_basics):
+
+	url = 'http://data.10jqka.com.cn/financial/sgpx/op/code/code/'+stock_code+'/ajax/1/'
+	resp = requests.get(url)
+
+	table = pd.read_html(resp.text)[0]
+	num = len(table)
+	if num >= 16:
+		name = stock_basics[stock_basics.index == stock_code]['name'].values[0]
+		print(str(stock_code)+'\t'+name)
 
 try:
 	stock_basics = ts.get_stock_basics()
@@ -15,17 +27,10 @@ except:
 	sys.exit(1)
 
 stock_list = stock_basics.index.values
-for stock_code in sorted(stock_list):
-#stock_code = sys.argv[1]
-	url = 'http://data.10jqka.com.cn/financial/sgpx/op/code/code/'+stock_code+'/ajax/1/'
-	resp = requests.get(url)
 
-	table = pd.read_html(resp.text)[0]
-	num = len(table)
-	#list = table[['股票代码','股票简称','股权登记日']][table.index == 0].values
-	if num >= 16:
-		#code,name,date = list[0]
-		name = stock_basics[stock_basics.index == stock_code]['name'].values[0]
-		print(str(stock_code)+'\t'+name)
-	#print(table[['股票代码','股票简称','股权登记日']])
-	#print(len(table))
+pool = multiprocessing.Pool(processes=4)
+for stock_code in sorted(stock_list):
+	#do_it(stock_code,stock_basics)
+	pool.apply_async(do_it, (stock_code,stock_basics))
+	pool.close()
+	pool.join()
