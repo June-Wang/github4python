c42c99167c34cef6367e5a6688bea3aca1d6cc67
Alex Wang <qq5910225@gmail.com>
2016-07-04 16:58:41 +0800

    ddd

--- forgood.sh
+++ forgood.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-file="$1"
-cat ${file} |\
-while read code name
-do 
-	grep "${code}" ./nxx.list >/dev/null &&\
-	echo "${code} ${name}"
+search_list="$1"
+for good_list in share.list short.list 
+do
+#good_list="$1"
+	cat ${good_list}|grep -oP '\d{6}'|\
+	while read code
+	do 
+		grep "${code}" ${search_list}
+		#echo "${code} ${name}"
+	done
 done
