ff320404d092b66fff78877dc6616955a77cabe9
Alex Wang <qq5910225@gmail.com>
2016-07-04 14:39:02 +0800

    筛选配股分红

--- bs/stock_shares.py
+++ bs/stock_shares.py
@@ -5,16 +5,27 @@ import sys
 import os
 import requests
 import pandas as pd
+import tushare as ts
 #from bs4 import BeautifulSoup
 
-stock_code = sys.argv[1]
-#url = 'http://data.10jqka.com.cn/financial/sgpx/op/code/code/600519/ajax/1/'
-url = 'http://data.10jqka.com.cn/financial/sgpx/op/code/code/'+stock_code+'/ajax/1/'
-resp = requests.get(url)
-#bsobj = BeautifulSoup(resp.text,"lxml")
-#print(bsobj)
-#print(bsobj.text)
+try:
+	stock_basics = ts.get_stock_basics()
+except:
+	print('timeout!')
+	sys.exit(1)
 
-table = pd.read_html(resp.text)[0]
-print(table[['股票代码','股票简称','股权登记日']])
-print(len(table))
+stock_list = stock_basics.index.values
+for stock_code in sorted(stock_list):
+#stock_code = sys.argv[1]
+	url = 'http://data.10jqka.com.cn/financial/sgpx/op/code/code/'+stock_code+'/ajax/1/'
+	resp = requests.get(url)
+
+	table = pd.read_html(resp.text)[0]
+	num = len(table)
+	#list = table[['股票代码','股票简称','股权登记日']][table.index == 0].values
+	if num == 17:
+		#code,name,date = list[0]
+		name = stock_basics[stock_basics.index == stock_code]['name'].values[0]
+		print(str(stock_code)+'\t'+name)
+	#print(table[['股票代码','股票简称','股权登记日']])
+	#print(len(table))
