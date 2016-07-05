a2e0413734fe33ce88d12d19535560c5d6637fcd
Alex Wang <qq5910225@gmail.com>
2016-07-05 17:38:35 +0800

    ddd

--- long_history.py
+++ long_history.py
@@ -20,28 +20,19 @@ def get_color(text):
 		my_text = text
 	return(my_text)
 
-#def get_basics_info(code,basics):
-#	stock_basics = {}
-#	name = basics[basics.index == code][['name']].values[0]
-#	industry = str(basics[basics.index == code][['industry']].values[0][0]) #行业
-#	area = str(basics[basics.index == code][['area']].values[0][0]) #区域
-#	pe = str(basics[basics.index == code][['pe']].values[0][0]) #市盈率
-#	try:
-#		pb = str(basics[basics.index == code][['pb']].values[0][0]) #市净率
-#	except:
-#		pb = 0.0
-#	stock_basics[code] = {'name':name,'industry':industry,'area':area,'pe':pe,'pb':pb}
-#	return(stock_basics[code])
-
 def get_price_info(code,df,day_count):
 	date = df.index.values[day_count]
+	ten_days_ago = df.index.values[day_count+10]
+	#print(date,ten_days_ago)
 	price = {}
 	price_open = df[df.index == date].open[0] #开盘价格
-	price_close = df[df.index == date].close[0] #开盘价格
+	price_close = df[df.index == date].close[0] #收盘价格
+	price_close_ten = df[df.index == ten_days_ago].close[0] #十天前的收盘价格
 	price_min = df[df.index == date].low[0] #当日最低
 	price_max = df[df.index == date].high[0] #当日最高
 	p_change = df[df.index == date].p_change[0] #当日股票涨幅
-	price[code] = {'open':price_open,'close':price_close,'min':price_min,'max':price_max,'p_change':p_change}
+	persent = (price_close - price_close_ten)/price_close_ten * 100
+	price[code] = {'open':price_open,'close':price_close,'min':price_min,'max':price_max,'p_change':p_change,'persent10':persent}
 	return(price[code])
 
 def get_data_list(df,day_list,day_count):
@@ -63,26 +54,6 @@ def get_data_list(df,day_list,day_count):
 			break
 	#print(len(data_list))
 	return(data_list,len(data_list))
-
-#def p_change_persent(df):
-#	count = 0
-#	num4up = 0
-#	for date_today in df.index.values:
-#		date_yestoday=str(datetime.datetime.strptime(date_today, '%Y-%m-%d') + datetime.timedelta(days = -1))[:10]
-#		#print(date_today,date_yestoday)
-#		try:
-#			p_change = df[df.index == date_today].p_change[0]
-#		except:
-#			continue
-#
-#		if p_change != p_change:
-#			continue
-#		
-#		count +=1
-#		if p_change >0:
-#			num4up +=1
-#	up_persent = num4up/count*100
-#	return(up_persent)
 
 def rules(df,day_list,data_list_dict,p_change):
 	num = len(day_list) +1
@@ -117,13 +88,13 @@ def color(color,mid_msg,end_msg):
 		print(Fore.GREEN+mid_msg+Style.RESET_ALL+'\t'+end_msg)
 
 def color4msg(df,code,day_count,price_dict,sh_price_dict,persent,sh_persent):
-	#date = str(yestoday)[:10]
 	
 	date = df.index.values[day_count]
 	head_msg = date + '\t'+'P(min/max/close):'
 	mid_msg = head_msg+'\t'+("%.2f" % price_dict[code]['min'])+'\t'+("%.2f" % price_dict[code]['max'])+'\t'+("%.2f" % price_dict[code]['close'])
 	persent_msg = get_color(("%.2f" % persent))+'\t'+get_color(("%.2f" % sh_persent))+'\t'+str(int(sh_price_dict['close']))
-	p_change_msg = get_color("%.2f" % price_dict[code]['p_change'])+'\t'+get_color("%.2f" % sh_price_dict['p_change'])
+	p_change_msg = get_color("%.2f" % price_dict[code]['p_change'])+'\t'+\
+		get_color("%.2f" % sh_price_dict['p_change'])+'\t'+get_color("%.2f" % price_dict[code]['persent10'])
 	end_msg = persent_msg+'\t'+p_change_msg
 
 	if persent <=-85:
@@ -185,7 +156,7 @@ if __name__ == "__main__":
 
 	colorama.init()
 	stock_code = sys.argv[1]
-	num4days = 400
+	num4days = 300
 
 	day_list = [i for i in range(5,185,5)]
 	day_list.append(3)
