39002942a8513eac0f00e5ae37c4331bf093fea8
Alex Wang <qq5910225@gmail.com>
2016-07-07 08:59:06 +0800

    ddd

--- bs/stock_shares.py
+++ bs/stock_shares.py
@@ -3,6 +3,7 @@
 import re
 import sys
 import os
+import time
 import requests
 import pandas as pd
 import tushare as ts
@@ -14,9 +15,21 @@ def do_it(stock_code,stock_basics):
 	url = 'http://data.10jqka.com.cn/financial/sgpx/op/code/code/'+stock_code+'/ajax/1/'
 	resp = requests.get(url)
 
-	table = pd.read_html(resp.text)[0]
+	try:
+		table = pd.read_html(resp.text)[0]
+	except:
+		return()
+	year_list = [ year[0].split("-")[0] for year in table[['预案公布日']].values]
+	local_year = time.strftime("%Y", time.localtime())
+
+	count = 0
 	num = len(table)
-	if num >= 16:
+	get_year_list = [int(local_year) - i for i in range(num-1)]
+	for year in year_list:
+		if year in get_year_list:
+			count +=1
+	if count == num and num>0:
+	#if local_year in year_list:
 		name = stock_basics[stock_basics.index == stock_code]['name'].values[0]
 		industry = stock_basics[stock_basics.index == stock_code]['industry'].values[0]
 		print(str(stock_code)+'\t'+name+'\t'+industry)
@@ -29,9 +42,5 @@ except:
 
 stock_list = stock_basics.index.values
 
-#pool = multiprocessing.Pool(processes=3)
 for stock_code in sorted(stock_list):
 	do_it(stock_code,stock_basics)
-#	pool.apply_async(do_it, (stock_code,stock_basics))
-#	pool.close()
-#	pool.join()
