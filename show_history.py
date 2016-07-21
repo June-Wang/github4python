4c7b6cbb0ca4c0fcb3b126dac0b750e6ee47210d
Alex Wang <qq5910225@gmail.com>
2016-07-21 16:10:22 +0800

    ddd

--- show_history.py
+++ show_history.py
@@ -169,7 +169,7 @@ persent,sh_persent,count_list,days_list_persent,year_list):
 	#day_sum = len(days_list_persent)
 	
 	#if #persent <=-85 and sh_persent <=0 and down_count == 1 and up_count == 1 and min_count ==1: # or \
-	if	((persent <= 30 and sh_persent <= -85) or (persent <=-85 and sh_persent <=0)) and \
+	if	((persent <= -85 and sh_persent <= -90) or (persent <=-85 and sh_persent <=0)) and \
 		(down_count == 1 and up_count == 1 and min_count ==1 and \
 		persent <=0 and sh_persent <=0 and \
 		(price_dict[code]['p_change'] <=0 and price_dict[code]['p_change'] >-9.5) and \
