a3f6acfc685abf7574d0fd6ff2b1ef1b34d5707d
Alex Wang <qq5910225@gmail.com>
2017-01-12 16:40:08 +0800

    6666

--- 747.py
+++ 747.py
@@ -43,6 +43,50 @@ def get_weight(stock_code,start_day,end_day):
 			mark2weight += 1
 	return(mark2weight)
 
+def get_data_list(df,day_list):
+	change_sum = 0.0
+	count = 0
+	data_list = {}
+	#for my_date in date_list:
+	for date in df.index.values:
+		try:
+			change_tmp = float(df[df.index == date].p_change[0])
+		except:
+			change_tmp = 0.0
+		change_sum += change_tmp
+		count = count +1
+		if count in day_list:
+			data_list[count] = change_sum
+	return(data_list)
+
+def rules(day_list,data_list_dict,p_change):
+	num = len(day_list) +1
+	count = 0
+
+	if p_change >=0:
+		count = count +1
+	else:
+		count = count -1
+
+	for day in day_list:
+		if  data_list_dict[day] >= 0:
+			count =count +1
+		else:
+			count =count -1
+	persent =  count / num * 100
+	return(persent)
+
+def get_price_info(code,df):
+	date = df.index.values[0]
+	price = {}
+	price_open = df[df.index == date].open[0] #开盘价格
+	price_close = df[df.index == date].close[0] #开盘价格
+	price_min = df[df.index == date].low[0] #当日最低
+	price_max = df[df.index == date].high[0] #当日最高
+	p_change = df[df.index == date].p_change[0] #当日股票涨幅
+	price[code] = {'open':price_open,'close':price_close,'min':price_min,'max':price_max,'p_change':p_change}
+	return(price[code])
+
 if __name__ == "__main__":
 	colorama.init()
 
@@ -83,11 +127,33 @@ if __name__ == "__main__":
 	#print(down2list)
 
 	num4days = 300
+	day_list = [i for i in range(5,185,5)]
+	day_list.append(3)
 	start_day = now - datetime.timedelta(days=num4days+max(day_list)+100)
-	try:
-		df = ts.get_hist_data(code,start=str(start_day),end=str(end_day))
-	except:
-		print('get_hist_data timeout!')
-		sys.exit(1)
+	days_list_persent = [3,5,10]
+
 	for stock_code in down2list:
-		
+		try:
+			df_hist_data = ts.get_hist_data(stock_code,start=str(start_day),end=str(end_day))		
+		except:
+			print('get_hist_data timeout!')
+			sys.exit(1)
+
+		lastday = df_hist_data.index.values[0]
+		price_dict = {}
+		price_dict[stock_code] = get_price_info(stock_code,df_hist_data)
+
+		if str(end_day) == str(lastday):
+			data_list_dict = get_data_list(df_hist_data,day_list)	
+			p_change = price_dict[stock_code]['p_change']
+			persent = rules(day_list,data_list_dict,p_change)
+			#print(persent)
+			
+			date = str(end_day)[:10]
+			if persent >= -50:
+				name = stock_basics[stock_basics.index == stock_code][['name']].values[0][0]
+				industry = stock_basics[stock_basics.index == stock_code][['industry']].values[0][0]
+				close = ("%.2f" % price_dict[stock_code]['close'])
+				output_args = [date,stock_code,close,name,industry]
+				msg = '\t'.join(output_args)
+				print(msg)	
