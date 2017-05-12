8ec35047016a97b37a418b331bb2f5548a9dc5c1
Alex Wang <qq5910225@gmail.com>
2017-05-12 16:54:37 +0800

    666

--- show_show.py
+++ show_show.py
@@ -233,16 +233,18 @@ def do_it(code,start_day,end_day,day_list):
 		if persent == -100 and sh_persent <= -80 and w_data_avg == -100:
 			color('cyan',mid_msg,end_msg)
 			act_buy_list.append(price_dict[code]['close'])
-		elif sh_persent <= 70 and ((persent == -100 and w_data <= -90 and w_data_avg <= -90) or\
-			(persent <= -80 and w_data == -100 and w_data_avg == -100) or\
-			(persent <= -90 and w_data <= -90 and w_data_avg <= -90) or\
-			(w_data_grow_list[0] == -100 and w_data_grow_list[1] == -100 and persent <= -90) or\
-			(w_data_grow_list[0] <= -90 and w_data_grow_list[1] <= -90 and w_data <= -90 and w_data_avg <= -90 and persent <= -80)):
+		#elif sh_persent <= 70 and ((persent == -100 and w_data <= -90 and w_data_avg <= -90) or\
+		#	(persent <= -80 and w_data == -100 and w_data_avg == -100) or\
+		#	(persent <= -90 and w_data <= -90 and w_data_avg <= -90) or\
+		#	(w_data_grow_list[0] == -100 and w_data_grow_list[1] == -100 and persent <= -90) or\
+		#	(w_data_grow_list[0] <= -90 and w_data_grow_list[1] <= -90 and w_data <= -90 and w_data_avg <= -90 and persent <= -80)):
+		elif (persent < 0 and sh_persent < 0) and (w_data_grow_list[0] == -100 and w_data_grow_list[1] == -100):
 			color('magenta',mid_msg,end_msg)
 			act_buy_list.append(price_dict[code]['close'])
-		elif (((persent == 100 and sh_persent >= 90) or persent == 100) or\
-			(w_data_grow_list[0] == 100 and w_data_grow_list[1] == 100)) and\
-			w_data == 100:
+		#elif (((persent == 100 and sh_persent >= 90) or persent == 100) or\
+		#	(w_data_grow_list[0] == 100 and w_data_grow_list[1] == 100)) and\
+		#	w_data == 100:
+		elif persent == 100 and (w_data_list[0] == 100 and w_data_list[1] == 100) and (w_data_grow_list[0] == 100 and w_data_grow_list[1] == 100):
 			color('yellow',mid_msg,end_msg)
 			act_sell_list.append(price_dict[code]['close'])
 		elif price_dict[code]['p_change'] > 0:
