fa24648fe42ef36486680031def4da0d0eec8f69
Alex Wang <qq5910225@gmail.com>
2017-08-10 14:51:27 +0800

    666

--- list.py
+++ list.py
@@ -260,7 +260,7 @@ def do_it(code,start_day,end_day,day_list):
 	income_avg = price_avg + price_avg*0.25
 	price_msg = 'min/max/avg/now:\t'+("%.2f" % price_min)+'\t'+("%.2f" % price_max)+'\t'+("%.2f" % price_avg)+'\t'+("%.2f" % now_price)
 	#act_msg = 'buy/sell(20%):\t'+("%.2f" % act_min)+'\t'+("%.2f" % act_max)+'\t'+("%.2f" % income)
-	act_msg = 'buy/sell(30%):\t'+("%.2f" % act_min)+'\t'+("%.2f" % income_avg)
+	act_msg = 'buy/sell(25%):\t'+("%.2f" % act_min)+'\t'+("%.2f" % income_avg)
 	#income_msg = 'income:\t'+get_color(str(price_income))+' %'
 
 	#output_args = [code,price_msg,act_msg,income_msg]
