2d1a5e4636f0cf64c3f4a7de97b1401687366b7b
Alex Wang <qq5910225@gmail.com>
2018-03-08 16:05:11 +0800

    666

--- analyze.py
+++ analyze.py
@@ -9,6 +9,8 @@ import multiprocessing
 import tushare as ts
 import pandas as pd
 import numpy as np
+from colorama import Fore, Back, Style
+from termcolor import colored, cprint
 
 def get_end_day(now):
     """
@@ -23,11 +25,12 @@ def get_end_day(now):
         end_day = now - datetime.timedelta(days=1)
     return(end_day)
 
-def get_start_day(now,num4days,day_list):
+def get_start_day(now,num4days):
     """
     获取时间周期的开始时间
     """
-    start_day = now - datetime.timedelta(days=num4days+max(day_list))
+    #start_day = now - datetime.timedelta(days=num4days+max(day_list))
+    start_day = now - datetime.timedelta(days=num4days)
     return(start_day)
 
 def get_df_hist_data(stock_code,start_day,end_day):
@@ -42,7 +45,7 @@ def get_df_hist_data(stock_code,start_day,end_day):
         sys.exit(1)
     return(df_hist_data)
 
-def get_cycle_p_change_list(df_hist_data,day_list):
+def get_cycle_p_change_list(df_hist_data,day_list,cycle_time):
     """
     生成周期内涨跌幅的值[返回列表]
     """
@@ -56,17 +59,7 @@ def get_cycle_p_change_list(df_hist_data,day_list):
         #print(day_time,str(cycle_p_change_sum))
     return(cycle_p_change_list)
 
-def get_cycle_date_list(df_hist_data,day_list):
-    """
-    生成周期内交易日期[返回列表]
-    """
-    cycle_date_list = list()
-    for day in day_list:
-        day_time = df_hist_data.index.values[day]
-        cycle_date_list.append(day_time)
-    return(cycle_date_list)
-
-def get_p_change_grow_list(df_hist_data,day_list):
+def get_p_change_grow_list(df_hist_data,day_list,cycle_time):
     """
     获取周期内涨跌幅的和
     """
@@ -83,40 +76,59 @@ def get_p_change_grow_list(df_hist_data,day_list):
                 count = count - 1
         #print(count)
         result = count / len(p_change_list) * 100
-        p_change_grow_list.append(("%.2f" % result))
+        p_change_grow_list.append(int(result))
     return(p_change_grow_list)
+
+def get_color(text):
+    text_f = float(text)
+    if text_f > 0:
+        text_color = colored(text, 'red')
+    elif text_f < 0:
+        text_color = colored(text, 'green')
+    else:
+        text_color = text
+    return(text_color)
 
 if __name__ == "__main__":
 
-    #file = sys.argv[1]
-    #stock_list = get_stock_list(file)
-    stock_list = ['000998']
-    #print(stock_list)
-    #sys.exit(1)
+    #stock_list = ['000998']
 
-    num4days = 160
-    cycle_time = 30
-    day_list = [i for i in range(0,cycle_time+1)]
-    #day_list = [i for i in range(5,30)]
-    #print(day_list)
+    cycle_time = 20
+    day_range = 90
+
+    num4days = day_range + cycle_time*2 +60
+
+    day_list = [i for i in range(0,day_range+1)]
 
     now = datetime.date.today()
     end_day = get_end_day(now)
-    start_day = get_start_day(now,num4days,day_list)
+    start_day = get_start_day(now,num4days)
 
-    stock_code = '000998'
+    stock_code = '600188'
     df_hist_data = get_df_hist_data(stock_code,start_day,end_day)
-    #print(start_day,end_day)
+    print(start_day,end_day)
     #print(df_hist_data)
-    cycle_p_change_list = get_cycle_p_change_list(df_hist_data,day_list)
+    cycle_p_change_list = get_cycle_p_change_list(df_hist_data,day_list,cycle_time)
     print(cycle_p_change_list)
+    #print(len(cycle_p_change_list))
 
-    cycle_date_list = get_cycle_date_list(df_hist_data,day_list)
-    print(cycle_date_list)
+    #cycle_date_list = get_cycle_date_list(df_hist_data,day_list)
+    #print(cycle_date_list)
 
     #info_list = np.vstack(cycle_date_list) + np.vstack(cycle_p_change_list)
     #info = pd.Series((cycle_p_change_list),index = cycle_date_list)
     #print(info)
     
-    p_change_grow_list = get_p_change_grow_list(df_hist_data,day_list)
+    p_change_grow_list = get_p_change_grow_list(df_hist_data,day_list,cycle_time)
     print(p_change_grow_list)
+    #print(len(p_change_grow_list))
+
+    for day in day_list:
+        #persent = float(cycle_p_change_list[day])+float(p_change_grow_list[day])
+        date = df_hist_data.index.values[day]
+        price = df_hist_data[['close']].values[day][0]
+        #print(price)
+        ma20 = df_hist_data[['ma20']].values[day][0]
+        color_field = [cycle_p_change_list[day],p_change_grow_list[day]]
+        output_color = '\t'.join(get_color(str(field)) for field in color_field)
+        print(date+"\t"+str(price)+"\t"+output_color+"\t"+str(ma20))
