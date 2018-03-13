cbffa1e6393aa5b8b1d10d7d39ec211df99828c8
Alex Wang <qq5910225@gmail.com>
2018-03-13 13:13:21 +0800

    666

--- research.py
+++ research.py
@@ -100,18 +100,84 @@ def get_persent_dict(df_hist_data,day_list,cycle):
         persent_dict[day] = ("%.2f" % result)
     return(persent_dict)
 
+def job4persent(stock_code,stock_basics,start_day,end_day,day_list,cycle_time):
+    df_hist_data = get_df_hist_data(stock_code,start_day,end_day)
+    persent_dict = get_persent_dict(df_hist_data,day_list,cycle_time)
+    #print(persent_dict[0])
+    get_stock_info(stock_code,stock_basics,df_hist_data,end_day)
+    stock_info = get_stock_info(stock_code,stock_basics,df_hist_data,end_day)
+    #print(persent_dict[0])
+    stock_info.append(int(float(persent_dict[0])))
+    return(stock_info)
+
+def get_stock_basics():
+    """
+    获取所有股票的基本信息
+    """
+    try:
+        stock_basics = ts.get_stock_basics()
+    except:
+        print('get_stock_basics timeout!')
+        sys.exit(1)
+    return(stock_basics)
+
+def get_stock_info(stock_code,stock_basics,df_hist_data,end_day):
+    """
+    获取最后一天的股票信息
+    """
+    date = str(end_day)[:10]
+    name = stock_basics[stock_basics.index == stock_code][['name']].values[0][0]
+    industry = stock_basics[stock_basics.index == stock_code][['industry']].values[0][0]
+    lastday = df_hist_data.index.values[0]
+    close = ("%.2f" % df_hist_data[df_hist_data.index == lastday].close[0])
+    output_args = [date,stock_code,name,close,industry]
+    #print(msg)
+    return(output_args)
+
+def color_negative_red(val):
+    """
+    Takes a scalar and returns a string with
+    the css property `'color: red'` for negative
+    strings, black otherwise.
+    """
+    val = int(val)
+    if val > 0:
+        color = 'red'
+    elif val < 0:
+        color = 'green'
+    else:
+        color = 'black'
+    #color = 'red' if val > 0 elif < 0 'green' else 'black'
+    return 'color: %s' % color
+
 if __name__ == "__main__":
 
-    num4days = 90
+    file = sys.argv[1]
+    stock_list = get_stock_list(file)
+    stock_basics=get_stock_basics()
 
-    stock_code = sys.argv[1]
+    num4days = 90
+    cycle_time = 30
+    day_list = [i for i in range(cycle_time)]
+    #stock_code = sys.argv[1]
 
     now = datetime.date.today()
     end_day = get_end_day(now)
     start_day = get_start_day(now,num4days)
-    df_hist_data = get_df_hist_data(stock_code,start_day,end_day)
-    
-    day_list = [i for i in range(30)]
-    #cycle_p_change_list = get_cycle_p_change_list(df_hist_data,day_list,30)
-    persent_dict = get_persent_dict(df_hist_data,day_list,30)
-    print(persent_dict[0])
+
+    cpus = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=cpus)
+
+    results = []
+    for stock_code in sorted(stock_list):
+        persent = pool.apply_async(job4persent,(stock_code,stock_basics,start_day,end_day,day_list,cycle_time))
+        #results.append(result.get())
+        #print(stock_code,result.get())
+        results.append(persent.get())
+    pool.close()
+    pool.join()
+
+    #print(results)
+    df = pd.DataFrame(results,columns=['日期','代码','名称','价格','行业','权重'])
+    s = df.style.applymap(color_negative_red,subset=pd.IndexSlice[:, ['权重']]).render()
+    print(s)
