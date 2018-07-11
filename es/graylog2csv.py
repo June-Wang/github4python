b982d96ab673eb2ff21f69a7e1dbb7cf4abfadac
Alex Wang <qq5910225@gmail.com>
2018-07-11 13:08:46 +0800

    加入正则匹配、替换，调整时区

--- es/graylog2csv.py
+++ es/graylog2csv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import csv,datetime,sys
-import requests
+import csv,datetime,sys,re
+import requests,dateparser
 
 def make_time_url(frequency):
     #frequency=1
@@ -28,9 +28,27 @@ if __name__=="__main__":
     graylog_url='http://graylog.server.local:9000/api/search/universal/absolute/export?'
     search_condition='query=nginx_version%3A1.6.x&'
     request_fields='fields=timestamp%2Crequest%2Cresponse%2Cagent%2CTIME&decorate=true'
-    frequency=1
+    frequency = 1
     time_url = make_time_url(frequency)
     csv_url = graylog_url + search_condition + time_url + request_fields
-    print(csv_url)
+    #print(csv_url)
     data_list = get_csv_data(csv_url,'view','readonly')
-    print(data_list)
+    #print(data_list)
+    request_re = re.compile('\?.+$')
+    dup_re = re.compile('\/\/')
+    #匹配多个条件
+    ignore_re = re.compile("(^-|^API|^Java|yunjiankong|nagios|check_http|okhttp|HttpClient)")
+    for data in data_list[1:]:
+        agent = data[3]
+        if re.search(ignore_re,agent):
+            continue
+        dt = dateparser.parse(data[0])
+        try:
+            #调整时区
+            date = dt.astimezone().strftime('%Y-%m-%d %H:%M:%S')
+        except:
+            continue
+        request = re.sub(request_re,'',data[1])
+        request = re.sub(dup_re,'/',request)
+        response = data[2]
+        print(date,request,response,agent)
