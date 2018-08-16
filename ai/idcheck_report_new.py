626e5818fa8b6b3583c05eaf9615515611cd9f8a
xiaojun.wang <xiaojun.wang@advance.ai>
2018-08-16 10:51:38 +0800

    pd按列排序，重建数字索引

--- ai/idcheck_report_new.py
+++ ai/idcheck_report_new.py
@@ -9,9 +9,9 @@ def color_negative(val):
     the css property `'color: green'` for negative
     strings, black otherwise.
     """
-    if float(val) < -10:
+    if float(val) < 0:
         color = 'green' 
-    elif float(val) >= 10:
+    elif float(val) > 0:
         color = 'red' 
     else: 
         color = 'black'
@@ -23,10 +23,12 @@ def eprint(*args, **kwargs):
 def get_date(days):
     today = datetime.date.today()
     yesterday = today + datetime.timedelta(days = -1)
+    pt1 = yesterday
     end_time = today + datetime.timedelta(days = -2)
+    pt2 = end_time
     fordays = -1 - days
     start_time = today + datetime.timedelta(days = fordays)
-    return(str(yesterday),str(start_time),str(end_time))
+    return(str(pt1),str(pt2),str(start_time),str(end_time))
 
 def get_query(accountId,date_list,query_template):
     query_list = list()
@@ -36,19 +38,21 @@ def get_query(accountId,date_list,query_template):
             r = requests.get(url,timeout=2)
         except requests.exceptions.RequestException as error:
             eprint('accountId:',accountId,'message:',error)
-            return(accountId,-1,-1)
+            return(-1,-1,-1)
             #sys.exit(1)
         json_data = r.json()
         #print(json_data)
         if json_data['message'] != 'OK':
             eprint('accountId:',accountId,'message:',json_data['message'])
-            return(accountId,-1,-1)
+            return(-1,-1,-1)
         queryCount = json_data['data']['queryCount']
         query_list.append(queryCount)
     
-    query = query_list[0]
-    avg_query = query_list[1]/days
-    return(accountId,query,avg_query)
+    pt1_query = query_list[0]
+    pt2_query = query_list[1]
+    avg_query = query_list[2]/days
+    #print(pt1_query,pt2_query,avg_query)
+    return(pt1_query,pt2_query,avg_query)
 
 def get_accountId(file):
     if not os.path.isfile(file):
@@ -71,12 +75,12 @@ def get_accountId(file):
 
 if __name__ == "__main__":
 
-    url_query = 'http://1.1.1.1:9090/monitor/queryCount/getQueryCount?'
-    query_template = """startDate=%s&endDate=%s&serviceCategory=CHECK&accountId=%s"""
+    url_query = 'http://10.10.2.10:8900/monitor/queryCount/getQueryCount?'
+    query_template = """startDate=%s&endDate=%s&serviceCategory=IDENTITY_CHECK&accountId=%s"""
     
     days=7
-    yesterday,start_time,end_time = get_date(days)
-    date_list = [[yesterday,yesterday],[start_time,end_time]]
+    pt1,pt2,start_time,end_time = get_date(days)
+    date_list = [[pt1,pt1],[pt2,pt2],[start_time,end_time]]
     
     try:
         file = sys.argv[1]
@@ -86,23 +90,25 @@ if __name__ == "__main__":
     
     accountId_list = get_accountId(file)
     query_data = list()
-    #accountId = '3170330'
     persent = 0.0
     for accountId in accountId_list:
-        accountId,query,avg_query = get_query(accountId,date_list,query_template)
+        pt1_query,pt2_query,avg_query = get_query(accountId,date_list,query_template)
         #print(accountId,str(query),str(avg_query))
         if avg_query == 0:
-            persent = int(query * 100)
-        elif query == -1:
+            persent = int(pt1_query * 100)
+        elif pt1_query == -1:
             persent = 0
         else:
-            persent = int((query-avg_query) / avg_query * 100)
-        query_data.append([accountId,query,("%.2f" % avg_query),persent])
+            persent = int((int(pt1_query) - avg_query) / avg_query * 100)
+        query_data.append([accountId,pt1_query,pt2_query,("%.2f" % avg_query),persent])
         #print(accountId,str(query),str(avg_query),str(persent))
     #print(query_data)
-    df = pd.DataFrame(query_data,columns=['accountId','count','avg(7d)','ratio'])
-    df.index = df.index + 1
-    html = df.style.set_properties(**{'background-color': '#D2D8F9',
+    df = pd.DataFrame(query_data,columns=['AccountId','PT1','PT2','AVG(7d)','Ratio'])
+    s = df.sort_values(by='PT1',ascending=False).reset_index()
+    s.index = s.index + 1
+    #print(s)
+    #sys.exit(1)
+    html = s.style.set_properties(**{'background-color': '#E5E7E9',
                            'color': '#000000',
-                           'border-color': 'white'}).applymap(color_negative,subset=pd.IndexSlice[:, ['ratio']]).render()
+                           'border-color': 'white'}).applymap(color_negative,subset=pd.IndexSlice[:, ['Ratio']]).render()
     print(html)
