4f0710e9bd40ec66c09eae4657424da46f9826ad
Alex Wong <5910225@qq.com>
2020-03-08 11:08:00 +0800

    add

--- new/make_new.sh
+++ new/make_new.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-code="$1"
-
-./newone.py ${code}|\
-awk -F',' '/^[0-9]/{print "tushare_lite,ts_code="$2",trade_date="$1" close="$3",weight="$4",vol="$5" "$NF}'
+./newone.py |\
+awk -F',' '/^[0-9]/{print "tushare_lite,ts_code="$1" trade_date="$2",pct_chg="$3",pct_chg_3d="$4",pct_chg_5d="$5",pct_chg_10d="$6",pct_chg_20d="$7",pct_chg_30d="$8",pct_chg_60d="$9",pct_chg_90d="$10",avg_w="$11",close="$(NF-3)",weight="$(NF-2)",vol="$(NF-1)" "$NF}'
