59680e8b032b38c961bbab490bc12db2e735c6c0
Alex Wong <5910225@qq.com>
2020-03-23 14:36:43 +0800

    del

--- new/make_new.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./newone.py |\
-awk -F',' '/^[0-9]/{print "tushare_lite,ts_code="$1" trade_date="$2",pct_chg="$3",pct_chg_3d="$4",pct_chg_5d="$5",pct_chg_10d="$6",pct_chg_20d="$7",pct_chg_30d="$8",pct_chg_60d="$9",pct_chg_90d="$10",avg_w="$11",amount="$12",close="$(NF-3)",weight="$(NF-2)",vol="$(NF-1)" "$NF}'
